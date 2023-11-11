73e41cfb4e3337b41e584d490881e03f21aab420
JuanMe16 <juanmena1605@gmail.com>
2023-11-11 10:24:32 -0500

    Development: Capacitor commands added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBBiMnGh33gZscGX5FHyN_RECyantwTnMc",
+    authDomain: "fir-uao-1cb89.firebaseapp.com",
+    projectId: "fir-uao-1cb89",
+    storageBucket: "fir-uao-1cb89.appspot.com",
+    messagingSenderId: "295425795170",
+    appId: "1:295425795170:web:5f4d8342148e0143945d3b"
+  }
 };
